7300bf13ed3de858798f8d410a15e8e26df826c1
Chauncey Pelton <chaunceypelton@gmail.com>
2024-06-28 07:33:41 +0700

    Can deal cards to players and table now

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -4,62 +4,63 @@ const val DIAMONDS = "\u2666"
 const val HEARTS = "\u2665"
 const val SPADES = "\u2660"
 const val CLUBS = "\u2663"
+const val ONE_HAND = 6
 
 val cardRanks = listOf<Any>('A', '2', '3', '4', '5', '6', '7', '8', '9', "10", 'J', 'Q', 'K')
 val cardSuits = listOf(DIAMONDS, HEARTS, SPADES, CLUBS)
 
 class CardDeck(cardRanks: List<Any>, cardSuits: List<String>) {
-    private var deck = mutableListOf<String>()
+    var cardsInDeck = mutableListOf<String>()
     init {
         for (suit in cardSuits) {
             for (rank in cardRanks) {
                 // print("$rank$suit ")
-                deck.add("$rank$suit")
+                cardsInDeck.add("$rank$suit")
             }
         }
     }
 
     fun reset() {
-        deck.clear()
+        cardsInDeck.clear()
         for (suit in cardSuits) {
             for (rank in cardRanks) {
-                deck.add("$rank$suit")
+                cardsInDeck.add("$rank$suit")
             }
         }
         println("Card deck is reset.")
     }
 
     fun shuffleDeck() {
-        for ((deckIndex, card) in deck.shuffled().withIndex()) {
-            deck[deckIndex] = card
+        for ((deckIndex, card) in cardsInDeck.shuffled().withIndex()) {
+            cardsInDeck[deckIndex] = card
         }
         println("Card deck is shuffled.")
     }
 
-    fun getCards() {
-        println("Number of cards:")
-        val cardsToGet = readln().toIntOrNull() ?: -1
-        if ((cardsToGet > 0) && (cardsToGet < 53)) {
-            if (cardsToGet > deck.size) {
-                println("The remaining cards are insufficient to meet the request.")
-            } else {
-                var cardsInHand = deck.take(cardsToGet)
-                deck.removeAll(deck.take(cardsToGet))
-                for (card in cardsInHand) {
-                    print("$card ")
-                }
-            }
-        } else {
-            println("Invalid number of cards.")
-        }
+    fun removeCardsFromDeck(cardsToRemove: Int): List<String> {
+        // Completely refactored because players will not be asking for specific numbers of cards.
+        // println("Number of cards:")
+        // Using toIntoOrNull to handle user input that can't be cast to Int.
+        // When cardsToGet can't be converted to Int, a default value of -1 is set so the else block can be reached.
+        // val cardsToGet = readln().toIntOrNull() ?: -1
+//        if ((cardsToDeal > 0) && (cardsToDeal < 53)) {
+//            if (cardsToDeal > cardsInDeck.size) {
+//                println("The remaining cards are insufficient to meet the request.")
+//            } else {
+                val takenCards = cardsInDeck.take(cardsToRemove)
+                cardsInDeck.removeAll(cardsInDeck.take(cardsToRemove))
+                return takenCards
+                // No longer have to display taken cards as they go to the table or a player.
+                // for (card in takenCards) {
+                //    print("$card ")
+                // }
+//            }
+//        } else {
+//            println("Invalid number of cards.")
+//        }
     }
 
-    fun displayCardsInDeck() {
-        // Display all cards currently in the deck.
-        for (card in deck) {
-            print("$card ")
-        }
-    }
+
 
     fun displayRanks() {
         // Print all ranks
@@ -88,8 +89,29 @@ class CardDeck(cardRanks: List<Any>, cardSuits: List<String>) {
 
 class GameHandler() {
     var keepPlaying = false
+    private var identifiedFirstPlayer = false
 
-    fun displayActionMenu(deck: CardDeck) {
+    fun printGameTitle() {
+        println("Indigo Card Game")
+    }
+
+    fun decideFirstPlayer(playerOne: Player, playerTwo: Player) {
+        while (!identifiedFirstPlayer) {
+            println("Play first?")
+            val playerAnswer = readln()
+            if (playerAnswer.lowercase() == "yes") {
+                playerOne.isFirstPlayer = true
+                playerTwo.isFirstPlayer = false
+                identifiedFirstPlayer = true
+            } else if (playerAnswer.lowercase() == "no") {
+                playerOne.isFirstPlayer = false
+                playerTwo.isFirstPlayer = true
+                identifiedFirstPlayer = true
+            }
+        }
+    }
+
+    fun displayActionMenu(deck: CardDeck, table: Table, playerOne: Player, playerTwo: Player) {
         println("Choose an action (reset, shuffle, get, exit):")
         val playerChoice = readln()
         if (playerChoice == "reset") {
@@ -97,14 +119,39 @@ class GameHandler() {
         } else if (playerChoice == "shuffle") {
             deck.shuffleDeck()
         } else if (playerChoice == "get") {
-            deck.getCards()
+            deck.removeCardsFromDeck(0)
         } else if (playerChoice == "exit") {
             keepPlaying = exitGame()
         } else if (playerChoice == "look") {
-            deck.displayCardsInDeck()
+            lookAtCards(deck, table, playerOne, playerTwo)
         } else {
             println("Wrong action.")
         }
+    }
+
+    fun dealCards(deck: CardDeck, table: Table, playerOne: Player, playerTwo: Player) {
+        table.cardsOnTable.addAll(deck.removeCardsFromDeck(4))
+        playerOne.cardsInHand.addAll(deck.removeCardsFromDeck(ONE_HAND))
+        playerTwo.cardsInHand.addAll(deck.removeCardsFromDeck(ONE_HAND))
+    }
+
+    private fun lookAtCards(deck: CardDeck, table: Table, playerOne: Player, playerTwo: Player) {
+        // Display all cards currently in play.
+        print("Cards remaining in the deck: ")
+        displayCards(deck.cardsInDeck)
+        print("Cards on the table: ")
+        displayCards(table.cardsOnTable)
+        print("Cards in Player One's hand: ")
+        displayCards(playerOne.cardsInHand)
+        print("Cards in Player Two's hand: ")
+        displayCards(playerTwo.cardsInHand)
+    }
+
+    private fun displayCards(cards: MutableList<String>) {
+        for (card in cards) {
+            print("$card ")
+        }
+        println()
     }
 
     private fun exitGame(): Boolean {
@@ -113,13 +160,30 @@ class GameHandler() {
     }
 }
 
+class Player() {
+    var cardsInHand = mutableListOf<String>()
+    var isFirstPlayer = true
+}
+
+class Table() {
+    var cardsOnTable = mutableListOf<String>()
+    private val startingTableCards = 4
+}
 
 fun main() {
     val cardDeck = CardDeck(cardRanks, cardSuits)
+    val playerOne = Player()
+    val playerTwo = Player()
+    val table = Table()
     val gameHandler = GameHandler()
 
+    gameHandler.printGameTitle()
+    gameHandler.decideFirstPlayer(playerOne, playerTwo)
+    gameHandler.dealCards(cardDeck, table, playerOne, playerTwo)
+
     while (!gameHandler.keepPlaying) {
-        gameHandler.displayActionMenu(cardDeck)
+
+        gameHandler.displayActionMenu(cardDeck, table, playerOne, playerTwo)
         println()
     }
 }
