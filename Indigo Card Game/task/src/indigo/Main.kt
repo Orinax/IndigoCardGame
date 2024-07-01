226f2fc0a3fdc3efcf36b7aa989298cdd8425e8a
Orinax <orinax@pop-os.localdomain>
2024-07-01 21:52:43 +0700

    Completed stage 4 of project

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -6,6 +6,7 @@ const val SPADES = "\u2660"
 const val CLUBS = "\u2663"
 const val ONE_HAND = 6
 const val STARTING_TABLE_CARDS = 4
+const val POINTS_FOR_MOST_CARDS = 3
 
 val cardRanks = listOf<Any>('A', '2', '3', '4', '5', '6', '7', '8', '9', "10", 'J', 'Q', 'K')
 val cardSuits = listOf(DIAMONDS, HEARTS, SPADES, CLUBS)
@@ -43,26 +44,9 @@ class CardDeck(cardRanks: List<Any>, cardSuits: List<String>) {
 
     /** Remove a number of cards from the deck and return a list of all cards that have been removed from the deck. */
     fun removeCardsFromDeck(cardsToRemove: Int): List<String> {
-        // Completely refactored because players will not be asking for specific numbers of cards.
-        // println("Number of cards:")
-        // Using toIntoOrNull to handle user input that can't be cast to Int.
-        // When cardsToGet can't be converted to Int, a default value of -1 is set so the else block can be reached.
-        // val cardsToGet = readln().toIntOrNull() ?: -1
-//        if ((cardsToDeal > 0) && (cardsToDeal < 53)) {
-//            if (cardsToDeal > cardsInDeck.size) {
-//                println("The remaining cards are insufficient to meet the request.")
-//            } else {
-                val takenCards = cardsInDeck.take(cardsToRemove)
-                cardsInDeck.removeAll(cardsInDeck.take(cardsToRemove))
-                return takenCards
-                // No longer have to display taken cards as they go to the table or a player.
-                // for (card in takenCards) {
-                //    print("$card ")
-                // }
-//            }
-//        } else {
-//            println("Invalid number of cards.")
-//        }
+        val takenCards = cardsInDeck.take(cardsToRemove)
+        cardsInDeck.removeAll(cardsInDeck.take(cardsToRemove))
+        return takenCards
     }
 
     /** Display all card ranks, Ace through King. */
@@ -96,6 +80,7 @@ class CardDeck(cardRanks: List<Any>, cardSuits: List<String>) {
 /** A class used to manage the game for players. Equivalent to a dealer in a real life card game. */
 class GameHandler() {
     var keepPlaying = false
+    val cardWinners = mutableListOf<String>()
     private var identifiedFirstPlayer = false
 
     /** Print the title of the game. */
@@ -189,26 +174,76 @@ class GameHandler() {
 
     /** Announce the number of cards on the table, and display the top card on the table. */
     fun announceCardsOnTable(table: Table) {
-        print("${table.cardsOnTable.size} cards on the table, and the top card is ${table.cardsOnTable.last()}")
-        println()
+        if (table.cardsOnTable.size == 0) {
+            println("No cards on the table")
+        } else {
+            print("${table.cardsOnTable.size} cards on the table, and the top card is ${table.cardsOnTable.last()}")
+            println()
+        }
+    }
+
+    /** Announce the current score and number of cards won for each player. */
+    fun announceCurrentScore(playerOne: Player, playerTwo: Player) {
+        println("Score: Player ${playerOne.score} - Computer ${playerTwo.score}")
+        println("Cards: Player ${playerOne.cardsWon} - Computer ${playerTwo.cardsWon}")
     }
 
     /** Check the played card to see if matches either rank or suit of the top table card. */
-    private fun checkCard(table: Table): List<Boolean> {
+    private fun checkCardMatch(table: Table): Boolean {
         val isMatch: Boolean
-        val isPoints: Boolean
-        val cardOnTopRank = table.cardsOnTable.elementAt(table.cardsOnTable.size - 2)[0]
-        val cardOnTopSuit = table.cardsOnTable.elementAt(table.cardsOnTable.size - 2)[1]
-        val cardJustPlayedRank = table.cardsOnTable.elementAt(table.cardsOnTable.size - 1)[0]
-        val cardJustPlayedSuit = table.cardsOnTable.elementAt(table.cardsOnTable.size - 1)[1]
-        if ((cardOnTopSuit == cardJustPlayedSuit) || (cardOnTopRank == cardJustPlayedRank)) {
-            isMatch = true
-            // return isMatch
-        } else {
-            isMatch = false
-            // return isMatch
+        if (table.cardsOnTable.size > 1) {
+            val cardOnTopRank = table.cardsOnTable.elementAt(table.cardsOnTable.size - 2)[0]
+            var cardOnTopSuit = table.cardsOnTable.elementAt(table.cardsOnTable.size - 2)[1]
+            val cardJustPlayedRank = table.cardsOnTable.elementAt(table.cardsOnTable.size - 1)[0]
+            var cardJustPlayedSuit = table.cardsOnTable.elementAt(table.cardsOnTable.size - 1)[1]
+            if (cardOnTopSuit == '0') {  // The suit for cards of rank 10 will be at index 2, not 1.
+                cardOnTopSuit = table.cardsOnTable.elementAt(table.cardsOnTable.size - 2)[2]
+            }
+            if (cardJustPlayedSuit == '0') {  // The suit for cards of rank 10 will be at index 2, not 1.
+                cardJustPlayedSuit = table.cardsOnTable.elementAt(table.cardsOnTable.size - 1)[2]
+            }
+            if ((cardOnTopSuit == cardJustPlayedSuit) || (cardOnTopRank == cardJustPlayedRank)) {
+                isMatch = true
+                return isMatch
+            } else {
+                isMatch = false
+                return isMatch
+            }
         }
-        if (())
+        return false
+    }
+
+    /** Check the cards on the table to see how many points there are in the set of cards. */
+    fun checkCardPoints(table: Table): Int {
+        var pointsEarned = 0
+        for (card in table.cardsOnTable) {
+            if (card.length == 3) {
+                pointsEarned++  // A card of length 3 is a 10 of any suit. This will always be 1 point.
+            } else {
+                if (card[0] == 'A' || card[0] == 'J' || card[0] == 'Q' || card[0] == 'K') {
+                    pointsEarned++
+                }
+            }
+        }
+        return pointsEarned
+    }
+
+    /** Handle playerOne's turn. */
+    private fun handlePlayerTurn(playerName: String, gameHandler: GameHandler, playerOne: Player, playerTwo: Player,
+                         table: Table) {
+        val cardMatch = checkCardMatch(table)
+        if (cardMatch) {
+            checkCardPoints(table)
+            if (playerName == "Player") {
+                playerOne.earnPoints(gameHandler, table)
+                cardWinners.add(playerOne.winCards(table))
+            } else {
+                playerTwo.earnPoints(gameHandler, table)
+                cardWinners.add(playerTwo.winCards(table))
+            }
+            println("${cardWinners.last()} wins cards")
+            announceCurrentScore(playerOne, playerTwo)
+        }
     }
 
     /** Turn pattern to use when playerOne is the first player. */
@@ -218,40 +253,42 @@ class GameHandler() {
         val exitNow = playerOne.playACard(table, gameHandler)
         println()
         if (!exitNow) {  // If false, gameplay will continue. If true, gameplay will end now.
-            var cardMatch = checkCard(table)
-            println("Does Player's card match?: $cardMatch")
+            handlePlayerTurn("Player", gameHandler, playerOne, playerTwo, table)
             gameHandler.announceCardsOnTable(table)
             playerTwo.playACard(table, gameHandler)
-            cardMatch = checkCard(table)
-            println("Does Computer's card match?: $cardMatch")
+            handlePlayerTurn("Computer", gameHandler, playerOne, playerTwo, table)
         }
     }
-
 
     /** Turn pattern to use when playerTwo is the first player. */
     fun playerTwoFirst(gameHandler: GameHandler, playerOne: Player, playerTwo: Player, table: Table) {
         gameHandler.announceCardsOnTable(table)
         playerTwo.playACard(table, gameHandler)
+        handlePlayerTurn("Computer", gameHandler, playerOne, playerTwo, table)
         gameHandler.announceCardsOnTable(table)
         playerOne.checkCardsInHand()
         playerOne.playACard(table, gameHandler)
+        handlePlayerTurn("Player", gameHandler, playerOne, playerTwo, table)
         println()
     }
 }
 
 /** A class used to manage player cards and actions. */
-class Player(var playerType: Boolean) {
+class Player(playerType: Boolean) {
     var cardsInHand = mutableListOf<String>()
     var isFirstPlayer = true
     var isHumanPlayer: Boolean
     var score = 0
     var cardsWon = 0
+    val name: String
 
     init {
         if (playerType) {
             isHumanPlayer = true
+            name = "Player"
         } else {
             isHumanPlayer = false
+            name = "Computer"
         }
     }
 
@@ -300,8 +337,20 @@ class Player(var playerType: Boolean) {
         return exitNow
     }
 
-    fun earnPoints(player: Player, table: Table) {
+    /** Add points to a player's score */
+    fun earnPoints(gameHandler: GameHandler, table: Table) {
+        score += gameHandler.checkCardPoints(table)
+    }
 
+    /** Add cards to a player's count of cards won.
+     * Return: name of the player who won the cards.
+     */
+    fun winCards(table: Table): String {
+        for (card in table.cardsOnTable) {
+            cardsWon++
+        }
+        table.cardsOnTable.clear()
+        return name
     }
 }
 
@@ -339,6 +388,29 @@ fun main() {
         if ((playerOne.cardsInHand.size == 0) && (playerTwo.cardsInHand.size == 0)) {
             if (cardDeck.cardsInDeck.size == 0) {
                 gameHandler.announceCardsOnTable(table)
+                if (table.cardsOnTable.size > 0) {
+                    if (gameHandler.cardWinners.elementAt(gameHandler.cardWinners.size -1) == "Player") {
+                        gameHandler.checkCardPoints(table)
+                        playerOne.earnPoints(gameHandler, table)
+                        playerOne.winCards(table)
+                    } else {
+                        gameHandler.checkCardPoints(table)
+                        playerTwo.earnPoints(gameHandler, table)
+                        playerTwo.winCards(table)
+                    }
+                }
+                if (playerOne.cardsWon > playerTwo.cardsWon) {
+                    playerOne.score += POINTS_FOR_MOST_CARDS
+                } else if (playerOne.cardsWon == playerTwo.cardsWon) {
+                    if (playerOne.isFirstPlayer) {
+                        playerOne.score += POINTS_FOR_MOST_CARDS
+                    } else {
+                        playerTwo.score += POINTS_FOR_MOST_CARDS
+                    }
+                } else {
+                    playerTwo.score += POINTS_FOR_MOST_CARDS
+                }
+                gameHandler.announceCurrentScore(playerOne, playerTwo)
                 gameHandler.keepPlaying = gameHandler.exitGame()
             } else {
                 gameHandler.dealCards(cardDeck, playerOne, playerTwo)
